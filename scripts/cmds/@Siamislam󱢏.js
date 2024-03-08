e071a271637184282a64db721c846e7e27d211eb
Jatirabbu <161809521+Jatirabbu@users.noreply.github.com>
2024-03-09 00:56:48 +0600

    Update @Siamislam󱢏.js

--- scripts/cmds/@Siamislam󱢏.js
+++ scripts/cmds/@Siamislam󱢏.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "@Siamislam󱢏",
         version: "1.0",
-        author: "kivv",
+        author: "kiv",
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
