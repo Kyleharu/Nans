79e31eff0607049215b3de062436b7e8a9533b3f
Jatirabbu <161809521+Jatirabbu@users.noreply.github.com>
2024-03-26 09:11:56 +0600

    Update Up.js

--- scripts/cmds/Up.js
+++ scripts/cmds/Up.js
@@ -4,7 +4,7 @@ const os = require('os');
 
 module.exports = {
   config: {
-    name: 'uptime',
+    name: 'up',
     version: '1.0.0',
     author: 'Siam the frog>🐸',
     countDown: 0,
@@ -24,25 +24,25 @@ onStart: async function ({ message, event, usersData, threadsData }) {
      const munites = Math.floor((uptime % 3600) / 60);
      const second = Math.floor(uptime % 60);
      
-     const uptime = `${hours} hours ${munites} munites ${second} second`;
+     const arif = `${hours} hours ${munites} munites ${second} second`;
      
      const url = await global.utils.getStreamFromURL("https://tinyurl.com/2c6fp9ar");
    
      const now = moment();
-     const date = now.format('DD-MMMM-Y/hh.mm.ss A');
+     const riley = now.format('DD-MMMM-Y/hh.mm.ss A');
      
-     const round = `${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
-     const lado = await usersData.getAll();
-     const data = await threadsData.getAll(); 
-     const cpus = `${os.type()} ${os.release()}`;
-     const model = `${os.cpus()[0].model} (${os.cpus().length} cores)`
+     const veli = `${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
+     const putra = await usersData.getAll();
+     const loufi = await threadsData.getAll(); 
+     const luxion = `${os.type()} ${os.release()}`;
+     const rizky = `${os.cpus()[0].model} (${os.cpus().length} cores)`
      
      const endTime = Date.now();
-     const ping = endTime - startTime;
+     const raffa = endTime - startTime;
      
-     const frog = `Bot ping: ${ping}\n━━━━━━━━━━━\nUptime: ${uptime}\n━━━━━━━━━━━\nTotal User: ${lado.length}\n━━━━━━━━━━━\nTotal Group: ${data.length}\n━━━━━━━━━━━\nMemory: ${round}\nOS: ${cpus}\n━━━━━━━━━━━\nCPU: ${model}\n━━━━━━━━━━━\n\nDate: ${date}\n━━━━━━━━━━`
+     const frog = `Bot ping: ${raffa}\n━━━━━━━━━━━\nUptime: ${arif}\n━━━━━━━━━━━\n\nDate: ${riley}\n━━━━━━━━━━`
 
      message.reply("Wait boss🐸", event.messageID);
      message.reply({ body: frog, attachment: url }, event.threadID);
   },
- };
+};
